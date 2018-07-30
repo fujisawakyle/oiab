c438a82b2d3efc1666942258eb54b271fbec2501
fujisawakyle <fujisawakyle@gmail.com>
2018-07-29 23:17:31 -0700

    add back helmet

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -9,6 +10,9 @@ import Themed from '../css_config/Themed';
 const TemplateWrapper = ({ children }) => (
   <Themed>
     <div>
+      <Helmet title="One in a Billion">
+        {/* <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css" /> */}
+      </Helmet>
       <Header />
       {children()}
       <Footer />
